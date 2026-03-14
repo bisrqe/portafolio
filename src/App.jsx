9a05b551947762182d06e77a7fc64a9c6610e7c0
Bismarck Animas Roque <bisanimroque@gmail.com>
2026-03-14 10:57:41 -0600

    Added info

--- src/App.jsx
+++ src/App.jsx
@@ -388,7 +388,7 @@ function App() {
           </section>
         )}
 
-        {currentPath === '/personal-projects' && (
+        {currentPath === '/professional-projects' && (
           <ProjectsView 
             projects={projects}
           />
